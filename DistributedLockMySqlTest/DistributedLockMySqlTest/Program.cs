3fa325b66f84e6e06041b19f349b2b01ef21c1cc
tedekeroth <ted.ekeroth@gmail.com>
2021-10-18 16:36:12 +0200

    Add files via upload

--- DistributedLockMySqlTest/DistributedLockMySqlTest/Program.cs
+++ DistributedLockMySqlTest/DistributedLockMySqlTest/Program.cs
@@ -20,20 +20,17 @@ namespace DistributedLockMySqlTest
         {
             var lockProvider = new MySqlDistributedSynchronizationProvider($"server=127.0.0.1;port=3306;user=root;password=root;database=benchmarks;SslMode=None");
 
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 5; i++)
             {
                 ThreadPool.QueueUserWorkItem((obj) =>
                 {
-                    var wait = new Random().Next(10, 50);
-                    Thread.Sleep(wait);
                     string lockString = string.Join("", Enumerable.Repeat(0, 10).Select(n => (char)new Random().Next(30, 110)));
-                    Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff}\tThread: {obj}\tRequesting lock {lockString} after sleeping for {wait} ms");
+                    Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff}\tThread: {obj}\tRequesting lock {lockString}");
                     Stopwatch sw = Stopwatch.StartNew();
                     using (var redLock = lockProvider.AcquireLock(lockString, TimeSpan.FromSeconds(1)))
                     {
                         Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff}\tThread: {obj}\tGot lock  {lockString} (waited {sw.ElapsedMilliseconds})");
                     }
-                    Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff}\tThread: {obj}\tReleased lock");
                 }, i);
             }
         }
