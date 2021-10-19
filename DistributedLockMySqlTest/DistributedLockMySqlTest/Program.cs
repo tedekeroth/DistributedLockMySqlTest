d8e38f453d6095aa4860141ea6581f290012f4fd
tedekeroth <ted.ekeroth@gmail.com>
2021-10-19 09:02:18 +0200

    Add files via upload

--- DistributedLockMySqlTest/DistributedLockMySqlTest/Program.cs
+++ DistributedLockMySqlTest/DistributedLockMySqlTest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Medallion.Threading;
 using Medallion.Threading.MySql;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading;
@@ -11,13 +12,14 @@ namespace DistributedLockMySqlTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello DistributedLock.Mysql!");
-            DoMySqlLockTest();
+            // DoMySqlLockTest();
+            DoMySqlLockTestReuseKeys();
             Console.ReadLine();
         }
 
         private static void DoMySqlLockTest()
         {
+            Console.WriteLine("Hello DistributedLock.Mysql!");
             var lockProvider = new MySqlDistributedSynchronizationProvider($"server=127.0.0.1;port=3306;user=root;password=root;database=benchmarks;SslMode=None");
 
             for (int i = 0; i < 5; i++)
@@ -30,6 +32,36 @@ namespace DistributedLockMySqlTest
                     using (var redLock = lockProvider.AcquireLock(lockString, TimeSpan.FromSeconds(1)))
                     {
                         Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff}\tThread: {obj}\tGot lock  {lockString} (waited {sw.ElapsedMilliseconds})");
+                    }
+                }, i);
+            }
+            Console.WriteLine();
+        }
+
+
+        private static void DoMySqlLockTestReuseKeys()
+        {
+            Console.WriteLine("Hello DistributedLock.Mysql with predefined keys");
+
+            int max = 5;
+            var lockProvider = new MySqlDistributedSynchronizationProvider($"server=127.0.0.1;port=3306;user=root;password=root;database=benchmarks;SslMode=None");
+            List<string> keys = Enumerable.Repeat(0, max).Select(a => string.Join("", Enumerable.Repeat(0, 10).Select(n => (char)new Random().Next(30, 110)))).ToList();
+
+            
+            for (int i = 0; i < max; i++)
+            {
+                ThreadPool.QueueUserWorkItem((obj) =>
+                {
+                    var key = keys[(int)obj];
+
+                    for (int q = 0; q < 3; q++)
+                    {
+                        Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff}\tThread: {obj} (q={q})\tRequesting lock {key}");
+                        Stopwatch sw = Stopwatch.StartNew();
+                        using (var redLock = lockProvider.AcquireLock(key, TimeSpan.FromSeconds(1)))
+                        {
+                            Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff}\tThread: {obj} (q={q})\tGot lock  {key} (waited {sw.ElapsedMilliseconds})");
+                        }
                     }
                 }, i);
             }
